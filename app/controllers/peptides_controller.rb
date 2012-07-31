0bb9cb3beb431d774e019a745d789fec1914a38d
Angel Pizarro <angel@turaro.com>
2012-07-31 11:15:45 -0400

    added will_paginate gem and needed code for pagination of peptides

--- app/controllers/peptides_controller.rb
+++ app/controllers/peptides_controller.rb
@@ -2,7 +2,7 @@ class PeptidesController < ApplicationController
   # GET /peptides
   # GET /peptides.json
   def index
-    @peptides = Peptide.all
+    @peptides = Peptide.paginate(:page => :params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/peptide.rb
+++ app/models/peptide.rb
@@ -1,9 +1,10 @@
 class Peptide < ActiveRecord::Base
+  self.per_page = 25
   attr_accessible :more, :penalized_rp, :pep_seq, :rank_product
 
   def psms()
 	  #connect to db and create psm object
 	  return Psm.find_by_pep(pep_seq)
   end
-  
+
 end
