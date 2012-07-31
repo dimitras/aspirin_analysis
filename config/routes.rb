08d06cc423c9d426e6d425792e290d0080083b57
Angel Pizarro <angel@turaro.com>
2012-07-31 11:22:24 -0400

    changed routes for peptides controller to follow Rails conventions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 AspirinAnalysis::Application.routes.draw do
 
-  get "viewer/show"
+  # get "viewer/show"
 
-  get "peptides/show"
+  # get "peptides/show"
+  resources :peptides :only => [:index,:show]
 
-  match ':name' => 'viewer#show', :as => :view_page
-  
+
+  match ':name' => 'viewer#show', :as => :view_page, :format => "html"
+
   root :to => 'peptides#index'
-  
+
 end
